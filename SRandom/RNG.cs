2f0bc30dac35034b55ae932de8deb9902ce313bd
adam currie <tfxflrstx@gmail.com>
2019-10-23 12:40:56 -0400

    fixed comment

--- SRandom/RNG.cs
+++ SRandom/RNG.cs
@@ -6,11 +6,9 @@ namespace SuperRandom {
     internal interface RNG<T> {
 
         /// <summary>
-        ///     Returns a random value in the specified range.
+        ///     Returns a random value.
         /// </summary>
         T Next();
-
-        //todo: add range stuff here
 
     }
 }
